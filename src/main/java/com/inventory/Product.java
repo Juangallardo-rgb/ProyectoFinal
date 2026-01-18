97b88abdb8fc18a8bcaaaf204273302956f6cf47
Juan Esteban Gallardo Alava <75323396+Juangallardo-rgb@users.noreply.github.com>
2026-01-18 17:37:17 -0500

    Add Product entity

---
+++ src/main/java/com/inventory/Product.java
@@ -0,0 +1,26 @@
+package com.inventory;
+
+public class Product {
+
+    private final String name;
+    private final int quantity;
+    private final double price;
+
+    public Product(String name, int quantity, double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
